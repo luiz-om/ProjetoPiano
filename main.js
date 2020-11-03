6d14b609a798b1e7e5eb2654c7a3e808fe3143b8
Luiz Fernando de Oliveira Martins <luizfernando_martins@hotmail.com>
2020-11-02 22:01:48 -0400

    refatoração

--- main.js
+++ main.js
@@ -7,57 +7,53 @@ function playNote(event) {
     // console.log(event.target.dataset.key)
     // console.log(event.keyCode)
 
-let audioKeyCode = getKeyCode(event)
+    let audioKeyCode = getKeyCode(event)
 
-//console.log(audioKeyCode)
+    //console.log(audioKeyCode)
 
-// type or pressed key
-const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+    // type or pressed key
+    const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+    //if key exists
+    const isKeyExists = key
 
+    if (!isKeyExists) {
+        return
+    }
+    addPlayingClass(key)
+    //play audio
+    playAudio(audioKeyCode)
 
-//if key exists
-const isKeyExists = key
-
-if (!isKeyExists) {
-    return
-}
-addPlayingClass(key)
-//play audio
-playAudio(audioKeyCode)
-
-function addPlayingClass(key){
-    key.classList.toggle('playing')
-}
-
-
+    function addPlayingClass(key) {
+        key.classList.toggle('playing')
+    }
 }
 
 // keyboard type
-function getKeyCode(event){
+function getKeyCode(event) {
     let keyCode;
 
-const isKeyboard = event.type ==="keydown"
-if (isKeyboard) {
-    keyCode = event.keyCode
-}else{
-    keyCode = event.target.dataset.key
-}
-return keyCode
+    const isKeyboard = event.type === "keydown"
+    if (isKeyboard) {
+        keyCode = event.keyCode
+    } else {
+        keyCode = event.target.dataset.key
+    }
+    return keyCode
 }
 
-function playAudio(audioKeyCode){
+function playAudio(audioKeyCode) {
     const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
-audio.currentTime=0
-audio.play();
+    audio.currentTime = 0
+    audio.play();
 }
-function removePlayingClass(event){
-   event.target.classList.remove('playing')
+function removePlayingClass(event) {
+    event.target.classList.remove('playing')
 }
 
 // type or pressed key 
 keys.forEach(function (key) {
-key.addEventListener('click',playNote)
-key.addEventListener('transitionend', removePlayingClass)
+    key.addEventListener('click', playNote)
+    key.addEventListener('transitionend', removePlayingClass)
 })
 //addEventListener é um escutador eventos
 window.addEventListener("keydown", playNote)
